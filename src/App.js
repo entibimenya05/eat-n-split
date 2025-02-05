bb82144bba3e65af881a600f3c59654dd0b7f81d
emmanuel <entibimenya03@gmail.com>
2025-02-05 14:46:55 +0000

    finished building the satatic part of the application

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ export default function App() {
     <div classname="app">
       <div classname="sidebar">
         <FriendsList />
+
+        <FormAddFriend />
+        {/*reusing the Button with Add Friend as children props*/}
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -63,7 +68,48 @@ function Friend({ friend }) {
           You and {friend.name} are even ${Math.abs(friend.balance)}
         </p>
       )}
-      <button classname="button">Select</button>
+      {/*using the component Button with the children props*/}
+      <Button>Select</Button>
     </li>
+  );
+}
+//since we will need this button in many places,let's create one with the children props
+//here the children props is Select
+function Button({ children }) {
+  return <button classname="button">{children}</button>;
+}
+//building the forms
+function FormAddFriend() {
+  return (
+    <form classname="form-add-friend">
+      <label>👬 Friend Name</label>
+      <input type="text" />
+      <label>🌅 IMAGE URL</label>
+      <input TYPE="text" />
+      {/*here the children props is Add*/}
+      <Button>Add</Button>
+    </form>
+  );
+}
+function FormSplitBill() {
+  return (
+    <form classname="form-split-bill">
+      <h2>Split bill with X</h2>
+      <label>💰 Bill value</label>
+      <input TYPE="text" />
+
+      <label>🧍‍♂️ Your expense</label>
+      <input TYPE="text" />
+
+      <label>👬 X's expense</label>
+      <input TYPE="text" disabled />
+      <label>💰 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      {/*here the children props is Add*/}
+      <Button>Split bill</Button>
+    </form>
   );
 }
